7e187cf82268ccd3409117ae3ca89c1a8d8d686d
sanjanasw <sswitharanage@student.nsbm.ac.lk>
2021-10-23 19:36:48 +0530

    query files re structured and create sp to get stydents payment data

--- sql/Main.sql
+++ sql/Main.sql
@@ -1,27 +1,5 @@
 CREATE DATABASE CHMS;
 
-CREATE TABLE Students (
-    Id INT PRIMARY KEY IDENTITY(1,1),
-    Name VARCHAR(100) NOT NULL,
-    Nic_No VARCHAR(12) NOT NULL,
-    Address VARCHAR(250) NOT NULL,
-    Tp INT NOT NULL,
-    Email VARCHAR(100) NOT NULL,
-    University VARCHAR(80),
-    Room_Id INT NOT NULL,
-);
-
-INSERT INTO Students(Name, Nic_No, Address, TP, Email, University, Room_Id) VALUES
-('Mahinda Rajapaksha', '583181098v', '1, Madamulana Rd, Hambanthota', 0771729181, 'mahinda@slpp.com', 'NSBM', 1),
-('Gotabaya Rajapaksha', '653181018v', '2, Madamulana Rd, Hambanthota', 0771723481, 'gota@slpp.com', 'NSBM', 2),
-('Ranil Wickramasinghe', '603181028v', '922, 1st Lane, Colombo', 0771729281, 'ranil@unp.com', 'UOC', 3),
-('Sajith Premadasa', '683481098v', '21, Flowers Street, Hambanthota', 077172281, 'sajith@sjb.com', 'UOSJ', 18),
-('Harin Fernando', '723181098v', '1, Lovers Street, Badulla', 0771238181, 'harin@sjb.com', 'UOSJ', 18);
-
-ALTER TABLE Students
-ADD CONSTRAINT FK_Room_Id
-FOREIGN KEY (Room_Id) REFERENCES Rooms(Id);
-
 CREATE TABLE Users (
     Id INT PRIMARY KEY IDENTITY(1,1),
     UserName VARCHAR(100) NOT NULL,
@@ -34,12 +12,16 @@ CREATE TABLE Users (
     User_Role BIT NOT NULL,
 );
 
+
+
 CREATE TABLE Buildings (
     Id INT PRIMARY KEY IDENTITY(1,1),
     Name VARCHAR(20) NOT NULL
 );
 
 INSERT INTO Buildings(Name) VALUES ('Building|B|001'), ('Building|B|002'), ('Building|G|003');
+
+
 
 CREATE TABLE Rooms (
     Id INT PRIMARY KEY IDENTITY(1,1),
@@ -114,7 +96,53 @@ INSERT INTO Rooms(Building_Id, Room_Type, Rental) VALUES
 (3, 'D', 18000.00),
 (3, 'D', 18000.00);
 
-SELECT Room_Count, Reserved FROM 
-(SELECT COUNT(Id)  AS Room_Count FROM Rooms) As Room_Count UNION
-(SELECT COUNT(DISTINCT Room_Id) AS Reserved_Room_Count FROM Students) As Reserved)  x
+
+
+    SELECT COUNT(DISTINCT Id) AS Room_Count FROM Rooms 
+    SELECT COUNT(DISTINCT Room_Id) AS Reserved_Room_Count FROM Students;
+
+    SELECT * FROM Students
+
+
+SELECT COUNT(Id) AS Student_Count, Gender  FROM Students GROUP BY Gender;
+
+
+SELECT COUNT(Id) AS Student_Count, University, AVG(COUNT(Id)) AS Average FROM Students GROUP BY University
+
+SELECT COUNT(Id) AS Staff_Count FROM Users WHERE User_Role = 0;
+
+
+SELECT * FROM Buildings;
+
+SELECT * FROM Rooms;
+SELECT
+(SELECT b.Name FROM Buildings b WHERE b.Id = r.Building_Id) AS Building,
+COUNT(r.Id)
+AS ROOMS FROM Rooms r GROUP BY Building_Id;
+
+
+SELECT b.Name,
+(SELECT COUNT(r.ID) FROM Rooms r WHERE r.Building_Id = b.Id AND r.Room_Type = 'S') AS 'Single Total',
+(SELECT COUNT(x.ID) FROM Rooms x WHERE x.Building_Id = b.Id AND x.Room_Type = 'D') AS 'Double Total',
+(SELECT y.Free FROM(
+    SELECT COUNT(x.Room_Id) AS 'Free',x.Room_Type, x.Building_Id FROM (
+        SELECT DISTINCT s.Room_Id, r.Room_Type, r.Building_Id FROM Students s JOIN Rooms r ON s.Room_Id = r.Id)x
+        GROUP BY x.Building_Id, x.Room_Type
+    )y  WHERE y.Building_Id = b.Id AND y.Room_Type = 'S') AS 'Rented Single',
+(SELECT y.Free FROM(
+    SELECT COUNT(x.Room_Id) AS 'Free',x.Room_Type, x.Building_Id FROM (
+        SELECT DISTINCT s.Room_Id, r.Room_Type, r.Building_Id FROM Students s JOIN Rooms r ON s.Room_Id = r.Id)x
+        GROUP BY x.Building_Id, x.Room_Type
+    )y  WHERE y.Building_Id = b.Id AND y.Room_Type = 'D') AS 'Rented Double'
+ FROM Buildings b;
+
+
+SELECT COUNT(x.Room_Id) AS 'Free',x.Room_Type, x.Building_Id FROM (
+SELECT DISTINCT s.Room_Id, r.Room_Type, r.Building_Id FROM Students s JOIN Rooms r ON s.Room_Id = r.Id)x
+GROUP BY x.Building_Id, x.Room_Type;
+
+
+SELECT DISTINCT s.Room_Id, r.Room_Type, r.Building_Id FROM Students s JOIN Rooms r ON s.Room_Id = r.Id;
+
+
 
